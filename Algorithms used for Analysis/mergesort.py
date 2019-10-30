ff41ec36da2a7be57ce77dbf3d747dcc38fbe439
Lisa Hoff <lisast@nmbu.no>
2019-10-30 14:37:29 +0100

    Changes

--- Algorithms used for Analysis/mergesort.py
+++ Algorithms used for Analysis/mergesort.py
@@ -1,35 +1,34 @@
-# Python program for implementation of MergeSort
-def mergeSort(arr):
+def mergesort(arr):
     '''Merge sort works as ldksglksdf
     '''
     if len(arr) > 1:
         mid = len(arr) // 2  # Finding the mid of the array
-        L = arr[:mid]  # Dividing the array elements
-        R = arr[mid:]  # into 2 halves
+        l = arr[:mid]  # Dividing the array elements
+        r = arr[mid:]  # into 2 halves
 
-        mergeSort(L)  # Sorting the first half
-        mergeSort(R)  # Sorting the second half
+        mergesort(l)  # Sorting the first half
+        mergesort(r)  # Sorting the second half
 
         i = j = k = 0
 
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
+        # Copy data to temp arrays l[] and r[]
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                arr[k] = l[i]
                 i += 1
             else:
-                arr[k] = R[j]
+                arr[k] = r[j]
                 j += 1
             k += 1
 
         # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
+        while i < len(l):
+            arr[k] = l[i]
             i += 1
             k += 1
 
-        while j < len(R):
-            arr[k] = R[j]
+        while j < len(r):
+            arr[k] = r[j]
             j += 1
             k += 1
 
@@ -39,6 +38,6 @@ if __name__ == '__main__':
     arr = [12, 11, 13, 5, 6, 7]
     print("Given array is", end="\n")
     print(arr)
-    mergeSort(arr)
+    mergesort(arr)
     print("Sorted array is: ", end="\n")
     print(arr)
